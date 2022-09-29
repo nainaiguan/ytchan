128d2a8702c780dd9cbfede8dd32338523bee12d
chenyuteng <chenyuteng@xiaohongshu.com>
2022-09-29 14:31:28 +0800

    add test case in main

--- main.go
+++ main.go
@@ -2,49 +2,16 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
-	"ytChan/slowchan"
+	"ytChan/subchan"
 )
 
 func main() {
-	//ch, shut := subchan.Default()
-	//
-	//c := ch.Subscribe("cyt", 1024)
-	//
-	//for i := 0; i < 10; i++ {
-	//	go func(x int) {
-	//		_ = ch.Subscribe(strconv.Itoa(x), 1024)
-	//	}(i)
-	//	go func() {
-	//		ch.Send("1")
-	//	}()
-	//}
-	//go func() {
-	//	for {
-	//		x := <-c
-	//		fmt.Println(x)
-	//	}
-	//}()
-	//
-	//ch.Send("1")
-	//ch.Send("2")
-	//ch.Send("3")
-	//ch.Send("4")
-	//ch.Send("5")
-	//
-	//time.Sleep(5 * time.Second)
-	//
-	//ch.Close(shut)
+	ch, shut := subchan.Default()
 
-	ch, shut := slowchan.Default()
-	ch.Send(1)
-	ch.Send(2)
-	ch.Send(3)
-	ch.Send(4)
-	ch.Send(5)
-	fmt.Println(ch.History())
+	c := ch.Subscribe("cyt", 1024)
 
-	c := ch.Start(1024)
 	go func() {
 		for {
 			x := <-c
@@ -52,7 +19,40 @@ func main() {
 		}
 	}()
 
-	time.Sleep(10 * time.Second)
+	cond := sync.NewCond(&sync.Mutex{})
+	for i := 0; i < 1000; i++ {
+		go func(i int) {
+			cond.L.Lock()
+			cond.Wait()
+			ch.Send(i)
+			cond.L.Unlock()
+		}(i)
+	}
+
+	time.Sleep(3 * time.Second)
+	cond.Broadcast()
+
+	time.Sleep(5 * time.Second)
 
 	ch.Close(shut)
+
+	//ch, shut := slowchan.Default()
+	//ch.Send(1)
+	//ch.Send(2)
+	//ch.Send(3)
+	//ch.Send(4)
+	//ch.Send(5)
+	//fmt.Println(ch.History())
+	//
+	//c := ch.Start(1024)
+	//go func() {
+	//	for {
+	//		x := <-c
+	//		fmt.Println(x)
+	//	}
+	//}()
+	//
+	//time.Sleep(10 * time.Second)
+	//
+	//ch.Close(shut)
 }
