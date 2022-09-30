4349b9d23dcbb2ba6134f7be871b59a88c50d196
nainaiguan <634024776@qq.com>
2022-09-30 09:57:49 +0800

    Add: test case

--- test/test.go
+++ test/test.go
@@ -1,5 +1,18 @@
 package test
 
-func test() {
+import (
+	"fmt"
+	"ytchan/dftchan"
+)
 
+func test() {
+	ch, shut := dftchan.Default()
+	for i := 0; i < 100; i++ {
+		ch.Send("dhiahgurhwaigjhfdsakufgdysuakgbfruwyakbvykuafb")
+	}
+
+	fmt.Println(ch.History())
+	fmt.Println(ch.Pull(100))
+
+	ch.Close(shut)
 }
